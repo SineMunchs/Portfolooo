d6ef36c57f02f2b032e1c73e5aefc6d640451c1f
Sine <55555@dmjx.dk>
2024-04-15 15:58:23 +0200

    ye

--- src/views/ProjectOne.vue
+++ src/views/ProjectOne.vue
@@ -10,8 +10,17 @@ import ProjectThumber from '@/components/ProjectThumber.vue'
 </script>
 
 <template>   
- <div class="ml-1 grid grid-cols-2 row-2">
-    <ProjectThumber video="../../" link="/projects/6"></ProjectThumber>
- </div>
+<div class=" mt-10 ml-1 grid grid-cols-2">
+  
+  <a href="/projects/4" class="elite text-blå font-bold text-6xl hover:text-blue-600">BLENDER</a>
+
+  <h1 class="flex elite text-blå row-start-2">FORM, COLOR THEORY, TYPOGRAPHY, DRAWING, CREATIVITY, 3D MODELLING, PROGRAMMING
+  </h1>
+
+</div>
+
+<video class="justify-self-end h-60 w-96 row-span-2 col-span-4 row-start-1 row-end-4" autoplay loop muted>
+    <source src="../../Video/Panda.mp4" type="video/mp4">
+  </video>
 
 </template>
