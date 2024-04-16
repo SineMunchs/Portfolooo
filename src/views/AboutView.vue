8cb184938cc729022f3bb19a0d5c410ad78bc2c6
Sine <55555@dmjx.dk>
2024-04-16 12:36:33 +0200

    Text save about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,11 +2,11 @@
     <div class="  mt-10 ml-1 blue elite text-blå text-9xl" >S I N E  M U N C H
     </div>
     <div class=" p-2 ml-1 blue elite text-blå text-4xl" >
-    <h1>My educational path took off at KEA in the field of design and entrepreneurship, and now I’m exploring the digital depths at DMJX, diving into Coded Design since 2023. My passion lies in crafting designs that are as functional as they are appealing, bridging the gap between aesthetics and utility. 
+    <h1>My educational path took off at KEA in the field of design and entrepreneurship, and now I’m exploring the digital world at DMJX, diving into Coded Design since 2023. My passion lies in crafting designs that are as functional as they are appealing, bridging the gap between aesthetics and utility. 
  </h1>
     </div>
 <!--MAIL-->
-    <div class="flex flex-col absolute bottom-0 right-0 p-4 p-2 elite underline text-blå text-8xl">
+    <div class="flex flex-col absolute bottom-0 right-0 p-2 elite underline text-blå text-8xl">
     <div class="flex items-center grid-3 mb-4 ">
       <img src="../../images/mail.svg" alt="Email Icon" class="h-14 w-20 mr-2" />
       <a href="mailto:munchsine@gmail.com">munchsine@gmail.com</a>
@@ -17,10 +17,12 @@
       <img src="../../images/arrow.svg" alt="Instagram Icon" class="h-15 w-20 mr-2" />
       <span>Instagram</span>
     </a>
+    <!--LINkdin-->
     <a href="https://www.linkedin.com/in/sine-munch-b992461b8/" target="_blank" class="flex items-center mb-2">
       <img src="../../images/arrow.svg" alt="LinkedIn Icon" class="h-15 w-20 mr-2" />
       <span>LinkedIn</span>
     </a>
+    <!--BEHANCE-->
     <a href="https://www.behance.net/sinemunch1" target="_blank" class=" flex items-center">
       <img src="../../images/arrow.svg" alt="Behance Icon" class="h-15 w-20 mr-2" />
       <span>Behance</span>
--- src/views/ProjectOne.vue
+++ src/views/ProjectOne.vue
@@ -28,9 +28,6 @@ import ProjectThumber from '@/components/ProjectThumber.vue'
 </template>
 
 
-To apply a blue color overlay to a video that only clears upon mouse hover, you will need to use CSS for styling. Here’s how you can achieve this effect assuming you’re working within a framework that allows inline styling or you can modify the CSS directly. I’ll guide you through creating a CSS class for this purpose.
-
-First, define a CSS class that applies the blue overlay using ::before pseudo-element. When the user hovers over the video, this overlay should become transparent. Here's the CSS code:
 
 
-
+  
