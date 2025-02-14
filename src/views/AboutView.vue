1854e1bbc3bd87d3bf349459e8b60489684b4b81
SineMunchs <167076042+SineMunchs@users.noreply.github.com>
2025-02-14 13:27:22 +0100

    commit it

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,44 +1,44 @@
 <template>
-    <div class="mt-10 ml-1 blue elite text-blå text-9xl tracking-widest flex grid-cols-2" >SINE MUNCH
+    <div class="mt-20 ml-1 blue elite text-blå text-9xl tracking-widest flex grid-cols-2" >SINE MUNCH
       <img src="../../images/Smil.svg" alt="Smiley" class="h-8 w-8 grid col-start-2 hover:animate-spin">
       </div>
      
     
     
-    <div class=" t-6 md:mt-10 p-2 max-w-3xl" >
-    <p className="text-2xl md:text-4xl tracking-widest hyphens-manual text-blå">My educational path took off at KEA in the field of design and entrepreneurship, and now I’m exploring the digital world at <u>DMJX</u>, diving into <u>Coded Design</u> since 2023. My passion lies in crafting designs that are as functional as they are appealing, bridging the gap between aesthetics and utility. 
+    <div class=" t-6 md:mt-11 p-2 max-w-4xl" >
+    <p className="text-2xl md:text-4xl tracking-widest hyphens-manual text-blå">My educational path took off at KEA in the field of design and entrepreneurship, and now I’m exploring the digital world at <b>DMJX</b>, diving into <b>Coded Design</b> since 2023. My passion lies in crafting designs that are as functional as they are appealing, bridging the gap between aesthetics and utility. 
     </p>
     </div>
 
-    <div class="mt-8 md:mt-0 md:absolute md:bottom-0 md:right-0 bottom-0 right-0 p-2 elite underline text-blå text-9xl tracking-widest">
+    <div class="mt-50 md:mt-50 md:absolute md:bottom-0 md:right-0 p-2 space-y-4 elite underline text-blå text-9xl tracking-widest">
       
-      <!--MAIL
+    <!--LINKEDIN-->
+    
+    <a href="https://www.linkedin.com/in/sine-munch-b992461b8/" target="_blank" class="flex items-center mb-2 hover:animate-pulse">
+      <img src="../../images/arrow.svg" alt="LinkedIn Arrow" class="h-12 w-16 md:h-15 md:w-20" />
+      <span>LinkedIn</span>
+    </a>
+    <!--BEHANCE-->
+    <a href="https://www.behance.net/sinemunch1" target="_blank" class=" flex items-center hover:animate-pulse">
+      <img src="../../images/arrow.svg" alt="Arrow" class="h-12 w-16 md:h-15 md:w-20" />
+      <span>Behance</span>
+    </a>
+    <!--instagram-->
+    <a href="https://www.instagram.com/codedstuff/" target="_blank" class="flex items-center mb-2 hover:animate-pulse">
+      <img src="../../images/arrow.svg" alt="Instagram Arrow" class="h-12 w-16 md:h-15 md:w-20" />
+      <span>Instagram</span>
+    </a>
+  </div>
+
+        <!--MAIL
     <div class="flex items-center grid-3 mb-4 ">
       <img src="../../images/mail.svg" alt="Email Icon" class="h-14 w-20 mr-2 " />
       <a href="mailto:munchsine@gmail.com">munchsine@gmail.com</a>
     </div> 
     -->
     
-    <!--LINKEDIN-->
-    
-    <a href="https://www.linkedin.com/in/sine-munch-b992461b8/" target="_blank" class="flex items-center mb-2 hover:animate-pulse">
-      <img src="../../images/arrow.svg" alt="LinkedIn Arrow" class="h-15 w-20 mr-2" />
-      <span>LinkedIn</span>
-    </a>
-    <!--BEHANCE-->
-    <a href="https://www.behance.net/sinemunch1" target="_blank" class=" flex items-center hover:animate-pulse">
-      <img src="../../images/arrow.svg" alt="Arrow" class="h-15 w-20 mr-2 " />
-      <span>Behance</span>
-    </a>
-    <!--instagram-->
-    <a href="https://www.instagram.com/codedstuff/" target="_blank" class="flex items-center mb-2 hover:animate-pulse">
-      <img src="../../images/arrow.svg" alt="Instagram Arrow" class="h-20 w-25 mr-2" />
-      <span>Instagram</span>
-    </a>
-  </div>
 
 </template>
-
 
 <style>
 @media (min-width: 1024px) {
